7dd1cdd7fe5d14a8d449253235b919fb9a93e6d5
Sourav Banerjee <rjsourav88@gmail.com>
2022-09-13 19:06:13 +0530

    try2

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from "../actions"
+import { increment, decrement } from "../actions/action"
 function App() {
   const store = useSelector(state => state);
   const dispatch = useDispatch();
