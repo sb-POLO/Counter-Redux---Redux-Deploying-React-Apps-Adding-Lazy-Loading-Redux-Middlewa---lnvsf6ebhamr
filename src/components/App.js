7d61fdea1c2ad94017bfdf7e11a8ca2b16de3aed
Sourav Banerjee <rjsourav88@gmail.com>
2022-09-13 19:00:47 +0530

    try1

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from "../actions"
 function App() {
+  const store = useSelector(state => state);
+  const dispatch = useDispatch();
 
   return (
     <div id='main'>
-      
+      <h3 data-testid='counter'>{store.counter}</h3>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+    counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return { counter: state.counter + 1 };
+        case 'DECREMENT':
+            return { counter: state.counter - 1 };
+    }
+}
+
+export default reducer;
